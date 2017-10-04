654060154bbcf1318ad206c2baf453834b06d3d7
Jon Preece <jonpreece@hotmail.co.uk>
2017-10-04 06:12:01 +0100

    Added AWS PM2 deployment config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   apps: [{
     name: 'ketogenicfamily',
-    script: './ketogenicfamily/server/index.js'
+    script: './server/index.js'
   }],
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-13-59-74-13.us-east-2.compute.amazonaws.com',
-      key: '~/.ssh/ketogenicfamily.pem',
+      host: 'ec2-13-58-30-49.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/ketogenic-family.pem',
       ref: 'origin/master',
       repo: 'git@github.com:jpreecedev/ketogenicfamily.git',
-      path: '/home/ubuntu/ketogenicfamily',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      path: '/home/ubuntu/server',
+      'post-deploy': 'npm install && npm run build && pm2 startOrRestart ecosystem.config.js'
     }
   }
 }
