a2ccb6aa4c30abfea6f73b6a3d1344d8bbe11f65
Jon Preece <jonpreece@hotmail.co.uk>
2017-10-29 08:07:54 +0000

    Improve formatting around ingredients

--- plugins/filters.js
+++ plugins/filters.js
@@ -8,7 +8,22 @@ Vue.filter('formatDate', function (value) {
 })
 
 Vue.filter('formatNumber', function (value, fixed = 2) {
-  if (value) {
-    return Number(Number.parseFloat(value).toFixed(fixed))
+  const format = (value, symbol) => {
+    return Math.floor(value) > 0 ? Math.floor(value) + symbol : symbol
+  }
+
+  switch (value % 1) {
+    case 0.25:
+      return format(value, '¼')
+    case 0.33:
+      return format(value, '⅓')
+    case 0.5:
+      return format(value, '½')
+    case 0.66:
+      return format(value, '⅔')
+    case 0.75:
+      return format(value, '¾')
+    default:
+      return Number(Number.parseFloat(value).toFixed(fixed))
   }
 })
