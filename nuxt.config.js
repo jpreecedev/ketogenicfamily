306eed5ffca90af79077b4690121d9072853c2c2
Jon Preece <jonpreece@hotmail.co.uk>
2018-03-03 03:39:03 +0000

    Added debug flag

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,30 +1,27 @@
-const sitemap = require('./server/sitemap').sitemap
+const sitemap = require("./server/sitemap").sitemap;
 
-const title = 'KetogenicFamily.com'
-const hostname = 'https://ketogenicfamily.com'
-const description = 'KetogenicFamily.com - Follow our progress towards a permanent ketogenic lifestyle'
+const title = "KetogenicFamily.com";
+const hostname = "https://ketogenicfamily.com";
+const description =
+  "KetogenicFamily.com - Follow our progress towards a permanent ketogenic lifestyle";
 
 module.exports = {
+  debug: true,
   /*
   ** Headers of the page
   */
-  modules: [
-    '@nuxtjs/sitemap',
-    '@nuxtjs/rss'
-  ],
+  modules: ["@nuxtjs/sitemap", "@nuxtjs/rss"],
   plugins: [
-    { src: '~plugins/ga.js', ssr: false },
-    { src: '~plugins/facebook-share.js', ssr: false },
-    { src: '~plugins/twitter-share.js', ssr: false },
-    { src: '~plugins/filters.js', ssr: true },
-    { src: '~plugins/eventbus.js', ssr: true }
+    { src: "~plugins/ga.js", ssr: false },
+    { src: "~plugins/facebook-share.js", ssr: false },
+    { src: "~plugins/twitter-share.js", ssr: false },
+    { src: "~plugins/filters.js", ssr: true },
+    { src: "~plugins/eventbus.js", ssr: true }
   ],
   sitemap: {
     generate: false,
     cacheTime: 1000 * 60 * 15,
-    exclude: [
-      '/blog/components/*'
-    ],
+    exclude: ["/blog/components/*"],
     hostname,
     routes: sitemap.all
   },
@@ -37,39 +34,40 @@ module.exports = {
   head: {
     title,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: description }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: description }
     ],
     script: [
-      { src: '//platform-api.sharethis.com/js/sharethis.js#property=5a06786850e8bc00116c7fea&product=sticky-share-buttons' },
-      { src: '/scripts/drip.js' }
+      {
+        src:
+          "//platform-api.sharethis.com/js/sharethis.js#property=5a06786850e8bc00116c7fea&product=sticky-share-buttons"
+      },
+      { src: "/scripts/drip.js" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
   ** Global CSS
   */
-  css: ['~/assets/styles/bootstrap.scss', '~/assets/styles/app.scss'],
+  css: ["~/assets/styles/bootstrap.scss", "~/assets/styles/app.scss"],
   /*
   ** Add axios globally
   */
   build: {
-    vendor: ['axios'],
+    vendor: ["axios"],
     /*
     ** Run ESLINT on save
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
       if (ctx.isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   }
-}
+};
